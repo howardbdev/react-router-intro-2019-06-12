b96fc272df1491860ca986c2f80e9be388f0af1d
howardbdev <howarddevennish@gmail.com>
2019-06-25 10:50:43 -0400

    refactor to functional component

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,45 +1,13 @@
 import React from 'react'
 import PlayerCard from './PlayerCard.js'
 
-const players = [
-  {
-    name: "Alex Morgan",
-    number: 12
-  },
-  {
-    name: "Carly Lloyd",
-    number: 30
-  },
-  {
-    name: "Crystal Dunne",
-    number: 20
-  },
-  {
-    name: "Alyssa Naeher",
-    number: 1
-  },
-  {
-    name: "Becky Sauerbrunn",
-    number: 4
-  },
-]
-
-class Players extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      players
-    }
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>PLAYERS</h2>
-        {this.state.players.map(p => <PlayerCard key={p.name} player={p}/>)}
-      </div>
-    )
-  }
+const Players = ({players}) => {
+  return (
+    <div>
+      <h2>PLAYERS</h2>
+      {players.map(p => <PlayerCard key={p.name} player={p}/>)}
+    </div>
+  )
 }
 
 export default Players
