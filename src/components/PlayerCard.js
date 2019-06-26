d2fae27d0a7fa54482ec4a6b9f178bd0b2e47d6b
howardbdev <howarddevennish@gmail.com>
2019-06-25 20:45:28 -0400

    change to functional component update refs

--- src/components/PlayerCard.js
+++ src/components/PlayerCard.js
@@ -1,22 +1,19 @@
 import React from 'react'
 
 
-class PlayerCard extends React.Component {
-
-  render(){
-      return (
-        this.props.player ?
-        <div>
-        <h4>{this.props.player.name} - {this.props.player.jerseyNumber}</h4>
-        <p>{this.props.player.position}</p>
-        <p>{this.props.player.birthday}</p>
-        <p>{this.props.player.caps}</p>
-        <p>{this.props.player.goals}</p>
-        <p>{this.props.player.club}</p>
-        </div> :
-        <h3>Player Card but no Player</h3>
-      )
-    }
+const PlayerCard = ({player}) => {
+    return (
+      player ?
+      <div>
+      <h4>{player.name} - {player.jerseyNumber}</h4>
+      <p>Pos: {player.position}</p>
+      <p>Bday: {player.birthday}</p>
+      <p>Caps: {player.caps}</p>
+      <p>Goals: {player.goals}</p>
+      <p>Club: {player.club}</p>
+      </div> :
+      <h3>Player Card but no Player</h3>
+    )
 }
 
 export default PlayerCard
