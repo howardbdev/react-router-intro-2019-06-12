dbc81c56ce65d5367bae22ec55c2aa20d5e8654e
howardbdev <howarddevennish@gmail.com>
2019-06-25 20:45:46 -0400

    update Players to handle its own resource

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,13 +1,31 @@
 import React from 'react'
-import PlayerCard from './PlayerCard.js'
-import { Link } from 'react-router-dom'
+import NewPlayerForm from './NewPlayerForm'
+import PlayerCard from './PlayerCard'
+
+import { NavLink, Switch, Route, Redirect } from 'react-router-dom'
 
 
-const Players = ({players}) => {
+const Players = ({players, handleSubmit}) => {
   return (
-    <div>
-      <h2>PLAYERS</h2>
-      {players.map(p => <PlayerCard key={p.name} player={p}/>)}
+    <div class="Players">
+      <div>
+        {players.map(p => <><NavLink key={p.jersey_number} to={`/players/${p.jersey_number}`}>{p.name}</NavLink><br/></>)}
+      </div>
+
+      <Switch>
+        <Route exact path="/players/new" render={props => <NewPlayerForm handleSubmit={handleSubmit} {...props}/>}/>} />
+
+        <Route
+          exact
+          path="/players/:jersey_number"
+          render={({ match }) => {
+            const playerJerseyNumber = match.params.jersey_number
+            const player = players.find(p => p.jersey_number === playerJerseyNumber)
+            return <PlayerCard player={player}/>
+          }
+        }/>
+
+      </Switch>
     </div>
   )
 }
